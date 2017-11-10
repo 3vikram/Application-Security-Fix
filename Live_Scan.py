6da25836efe93f7a82169abd1580851433b22875
3vikram <viku.vikram05@gmail.com>
2017-11-11 02:38:24 +0530

    Update Live_Scan.py

--- Live_Scan.py
+++ Live_Scan.py
@@ -5,6 +5,7 @@ def ping(userin):
     number_of_servers_up = 0
     number_of_servers_down = 0
     number_of_servers_failed_to_reach = 0
+    list_live_servers = []
     for hn in hostname:
         response= os.system('ping ' + hn)
         if response==0:
@@ -12,6 +13,7 @@ def ping(userin):
             number_of_servers_up+=1
             report=open('live_ip.xls','a+')
             report.write(str(hn)+'\n')
+            list_live_servers.append(hn)
         elif response==2:
             print("{} is down".format(hn))
             number_of_servers_down+=1
@@ -22,7 +24,7 @@ def ping(userin):
     print("Number of servers up {}".format(number_of_servers_up))
     print("Number of servers down {}".format(number_of_servers_down))
     print("Number of servers failed to reach {}".format(number_of_servers_failed_to_reach))
-
+    print("list of live servers are {}".format(list_live_servers))
 
 
 output=ping("C:\\Users\\trra\\Documents\\ip.txt")
