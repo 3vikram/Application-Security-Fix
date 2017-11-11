bd4dcf98073c837a7bf0d6cd028d2f9faad13942
3vikram <viku.vikram05@gmail.com>
2017-11-11 12:50:45 +0530

    Update XSS_URI.py

--- XSS_URI.py
+++ XSS_URI.py
@@ -1,13 +1,15 @@
 import requests
+import ast
 
 def xss_test(**url_params):
     file = input("Enter the xss payloads containing file to test: ")
     uri = input("Enter the uri to test: ")
     uri_parameters = input("Enter the uri parameter to test: ")
+    req_headers = ast.literal_eval(input("Enter the headers to be sent with the request: "))
     fl = open(file, 'r+')
     for payload in fl:
         url_params[uri_parameters] = payload.rstrip()
-        response= requests.get(uri, params=url_params)
+        response= requests.get(uri, params=url_params, headers=req_headers)
         if payload.rstrip() in str(response.content):
             print('Vulnerable to XSS')
             print('{} uri parameter is effected and the payload is {}'.format(uri_parameters, payload.rstrip()))
@@ -15,7 +17,8 @@ def xss_test(**url_params):
         else:
             print('Not Vulnerable to XSS')
     print("Test complete")
+    print(req_headers)
 
 
-#result = xss_test(vikram=160988, txtSearch='name', nack='t', id=190829301238, token='abl89430jaca90')
-#print(result)
+a = xss_test(txtSearch="123",a="test")
+print(a)
