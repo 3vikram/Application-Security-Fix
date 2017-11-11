e4dc4e2452a4fa31c33674ec1a379d69177b572b
3vikram <viku.vikram05@gmail.com>
2017-11-11 20:58:21 +0530

    Update XSS_URI.py

--- XSS_URI.py
+++ XSS_URI.py
@@ -6,10 +6,15 @@ def xss_test(**url_params):
     uri = input("Enter the uri to test: ")
     uri_parameters = input("Enter the uri parameter to test: ")
     req_headers = ast.literal_eval(input("Enter the headers to be sent with the request: "))
+    http_method = input("Enter the HTTP method: ").upper()
     fl = open(file, 'r+')
     for payload in fl:
         url_params[uri_parameters] = payload.rstrip()
-        response= requests.get(uri, params=url_params, headers=req_headers)
+        if http_method == "GET":
+            response = requests.get(uri, params=url_params, headers = req_headers)
+        elif http_method == "POST":
+            response = requests.post(uri, data=url_params, headers = req_headers)
+            print("POST method executed")
         if payload.rstrip() in str(response.content):
             print('Vulnerable to XSS')
             print('{} uri parameter is effected and the payload is {}'.format(uri_parameters, payload.rstrip()))
